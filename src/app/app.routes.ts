b44be55c1acb1160049d15200175160acd3b873c
bngams <n.boris@devidia.net>
2024-09-24 14:47:48 +0000

    define routes and add router-outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'details/:id', component: DetailsComponent },
+  // /!\ ** => capture all flags => at the end
+  { path: '**', component: NotFoundComponent },
+];

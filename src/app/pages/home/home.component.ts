2903a1de0c32192ed9e657adf560d08f9bed51d2
bngams <n.boris@devidia.net>
2024-09-24 10:03:30 +0000

    create type HousingLocation in models folder

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HousingLocationComponent } from "../../components/housing-location/housing-location.component";
+import { HousingLocation } from '../../models/housing-location';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,16 @@ import { HousingLocationComponent } from "../../components/housing-location/hous
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
 
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
